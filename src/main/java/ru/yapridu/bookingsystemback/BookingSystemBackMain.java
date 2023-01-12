7fb44d879dc76b4aa138d65e04d4dd6e2dfaa431
Denis Plekhanov <85961236+Imdizzza@users.noreply.github.com>
2023-01-12 22:24:15 +0300

    Task #3

---
+++ src/main/java/ru/yapridu/bookingsystemback/BookingSystemBackMain.java
@@ -0,0 +1,12 @@
+package ru.yapridu.bookingsystemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemBackMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemBackMain.class, args);
+	}
+}
---
+++ src/main/java/ru/yapridu/bookingsystemback/controller/AppVersionController.java
@@ -0,0 +1,29 @@
+package ru.yapridu.bookingsystemback.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Den
+ */
+
+@RestController
+@RequestMapping("/api/v1/version")
+@RequiredArgsConstructor
+@PropertySource("classpath:application.yml")
+public class AppVersionController {
+    @Autowired
+    private Environment env;
+
+    @GetMapping()
+    public ResponseEntity<String> getVersion() {
+        return new ResponseEntity<>("Application version is: " + env.getProperty("yapridu.version"), HttpStatus.OK);
+    }
+}
