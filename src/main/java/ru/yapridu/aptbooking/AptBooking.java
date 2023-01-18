05e248fcbc2d8f8dbdd03710c6d5beb8f0e54108
Denis Plekhanov <85961236+Imdizzza@users.noreply.github.com>
2023-01-18 08:24:15 +0300

    Task #2 (VK и Google authorisation)

--- src/main/java/ru/yapridu/aptbooking/AptBooking.java
+++ src/main/java/ru/yapridu/aptbooking/AptBooking.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AptBooking {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AptBooking.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AptBooking.class, args);
+    }
 }
