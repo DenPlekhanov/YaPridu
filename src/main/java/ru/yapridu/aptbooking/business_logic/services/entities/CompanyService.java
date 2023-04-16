4619870161cd18f71fba7c5180a855ee00aeeb37
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-04-16 08:31:20 +0300

    Added service

---
+++ src/main/java/ru/yapridu/aptbooking/business_logic/services/entities/CompanyService.java
@@ -0,0 +1,29 @@
+package ru.yapridu.aptbooking.business_logic.services.entities;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yapridu.aptbooking.business_logic.entities.Company;
+import ru.yapridu.aptbooking.business_logic.models.CreateCompanyDTO;
+import ru.yapridu.aptbooking.repositories_ebean.CompanyEbeanRepository;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    private final CompanyEbeanRepository repository;
+
+    public Company create(CreateCompanyDTO companyData) {
+
+        Company company = Company.builder()
+            .ownerId(companyData.getOwnerId())
+            .name(companyData.getName())
+            .address(companyData.getAddress())
+            .contact(companyData.getContact())
+            .officialDetails(companyData.getOfficialCompanyDetails())
+            .description(companyData.getDescription())
+            .build();
+
+        return repository.insert(company);
+    }
+
+}
