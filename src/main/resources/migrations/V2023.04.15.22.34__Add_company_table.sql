6e38b169682314174ee63d8963530fad39b38602
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-04-16 16:06:39 +0300

    Remove quotes

--- src/main/resources/migrations/V2023.04.15.22.34__Add_company_table.sql
+++ src/main/resources/migrations/V2023.04.15.22.34__Add_company_table.sql
@@ -8,11 +8,11 @@ CREATE TABLE company (
     official_details TEXT,
     description TEXT,
 
-    "created_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "modified_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "version" BIGINT NOT NULL DEFAULT 1,
+    created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    modified_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    version BIGINT NOT NULL DEFAULT 1,
 
-    CONSTRAINT "pk_company"
-        PRIMARY KEY ("id")
+    CONSTRAINT pk_company
+        PRIMARY KEY (id)
 
 );
