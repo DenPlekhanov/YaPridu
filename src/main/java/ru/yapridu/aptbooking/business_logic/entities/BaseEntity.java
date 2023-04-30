0e39dc8b9d34a59ff5daa6cbf5d77f8ffdcee0cf
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-04-30 20:51:52 +0300

    Fixed annotations for base entity

--- src/main/java/ru/yapridu/aptbooking/business_logic/entities/BaseEntity.java
+++ src/main/java/ru/yapridu/aptbooking/business_logic/entities/BaseEntity.java
@@ -6,16 +6,16 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import java.time.Instant;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 import java.util.Date;
 import java.util.UUID;
-
-import static javax.persistence.TemporalType.TIMESTAMP;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,26 +27,15 @@ public class BaseEntity {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(generator = "UUID")
+    @GeneratedValue
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(name = "id", updatable = false, nullable = false)
     private final UUID id;
 
-
-//    @WhenCreated
-//    private final Instant createdDate;
-
-//    @Temporal(TIMESTAMP)
-    @Column(name = "created_date", updatable = false)
+    @CreationTimestamp
     private final Date createdDate;
 
-//    @WhenModified
-//    private final Instant modifiedDate;
-
-//    @Temporal(TIMESTAMP)
-    @Column(name = "modified_date", insertable = false)
+    @UpdateTimestamp
     private final Date modifiedDate;
-
 
     @Version
     private final Long version;
