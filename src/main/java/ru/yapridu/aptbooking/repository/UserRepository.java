cd4e652db64121b4ff7f40ac6fd5a8087ec61139
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-04-30 20:39:52 +0300

    User removed

--- src/main/java/ru/yapridu/aptbooking/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.yapridu.aptbooking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.yapridu.aptbooking.business_logic.entities.UserOld;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserOld, Long> {
-}
