aba26320c4cce759fef605800d4c749a4f88d6ac
Denis Plekhanov <85961236+Imdizzza@users.noreply.github.com>
2023-06-12 05:38:00 +0300

    id field have been renamed.

--- src/main/java/ru/yapridu/aptbooking/exceptions/ErrorMessageContainer.java
+++ src/main/java/ru/yapridu/aptbooking/exceptions/ErrorMessageContainer.java
@@ -7,13 +7,13 @@ import java.util.UUID;
 @Getter
 public class ErrorMessageContainer {
 
-    private final UUID id;
+    private final UUID errorId;
     private final ErrorCode errorCode;
     private final OperationCode operationCode;
     private final String message;
 
     public ErrorMessageContainer(SystemException e) {
-        this.id = e.getId();
+        this.errorId = e.getId();
         this.errorCode = e.getErrorCode();
         this.operationCode = e.getOperationCode();
         this.message = e.getErrorCode().getDescription();
